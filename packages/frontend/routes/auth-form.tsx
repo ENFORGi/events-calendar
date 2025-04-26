88f05acf48be7b6babe1314d5952f3dce696a8f1
coffeequ <savvapugach2004@mail.ru>
2025-04-26 18:23:17 +0500

    Добавил форму для отправки пользовательских данных на сервер

--- packages/frontend/routes/auth-form.tsx
+++ packages/frontend/routes/auth-form.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { Button } from "react-native";
 
 interface IUser {
     mail: string,
@@ -7,16 +8,37 @@ interface IUser {
 
 export default function authForm(){
 
-    const [formData, setDataForm] = useState<IUser>();
+    const [formData, setDataForm] = useState<IUser>({
+        mail: "",
+        name: ""
+    });
+
+    const handleChangeMail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDataForm({...formData, mail: e.target.value})
+    }
+
+    const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDataForm({...formData, name: e.target.value})
+    }
+
+    const handleSend = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        const body = JSON.stringify(formData);
+        localStorage.setItem("user", body);
+        console.log(body);
+        //TODO: PUT || POST запрос к базе
+    }
 
     return(
-        <div style={}>
+        <div>
             <form>
                 <label>Почта</label>
-                <input onChange={() => {}}></input>
+                <input value={formData.mail} onChange={handleChangeMail} placeholder="Почта"></input>
                 <br/>
                 <label>ФИО</label>
-                <input onChange={() => {}}></input>
+                <input value = {formData.name} onChange={handleChangeName} placeholder="ФИО"></input>
+                <br/>
+                <button type="submit" onClick={handleSend}>Войти</button>
             </form>
         </div>
     );
