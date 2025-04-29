cc3ac2a39c6daf92aecbee70d7b7991cf6b6896b
coffeequ <savvapugach2004@mail.ru>
2025-04-29 10:32:45 +0500

    Календарь. Выпадающие списки

--- packages/frontend/app/(calendar)/calendar.tsx
+++ packages/frontend/app/(calendar)/calendar.tsx
@@ -1,19 +1,61 @@
 import { ivcArr } from "@/constants/ivc";
-import { useState } from "react";
-import { View, StyleSheet, Text, FlatList} from "react-native";
 
+import { useEffect, useState } from "react";
+
+import { View, StyleSheet, Text} from "react-native";
+import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
+
+import { monthArr } from "@/constants/month";
+
+interface IPropsListEvents{
+    label: string,
+    items: {
+        label: string,
+        value: string
+    }[]
+}
+ 
 export default function CalendarEvents() {
 
-    const [currentDate, setCurrentDate] = useState(Date.now());
+    //TODO: Лучше использовать хук useState? Или делать отдельную переменную для хранения значения года?
+    //const [currentYear, setCurrentYear] = useState(new Date(Date.now().valueOf()).getFullYear());
 
-    const [date, setDate] = useState(() => {
-        const formatDate = new Date(currentDate.valueOf()).toLocaleDateString("ru-RU", {
+    const [selectedMonth, setSelectedMonth] = useState(() => {
+        const formatDate = new Date().toLocaleDateString("ru-RU", {
             month: "long",
-            year: "numeric"
         });
 
-        return formatDate[0].toUpperCase() + formatDate.slice(1);
+        return `${formatDate[0].toUpperCase() + formatDate.slice(1)}, ${new Date().getFullYear()}`;
     });
+
+    const [yearAndMonth, setYearAndMonth] = useState<string[]>([]);
+
+    const [selectedIVC, setSelectedIVC] = useState(ivcArr[5]);
+
+    const [events, setEvents] = useState<IPropsListEvents[]>([]);
+
+    const [selectedEvent, setSelectedEvents] = useState(null);
+
+    useEffect(() => {
+        setYearAndMonth(monthArr.map(item => {
+            return `${item}, ${new Date(Date.now().valueOf()).getFullYear()}`;
+        }));
+
+        //TODO: Fetch запрос на ивенты с базы
+        const localEvents = [
+            {
+                label: "волонтерство",
+                items: [
+                    { label: "донорство крови1", value: "донорство крови1" },
+                    { label: "донорство крови2", value: "донорство крови2" },
+                    { label: "донорство крови3", value: "донорство крови3" }
+                ]
+            },
+        ];
+        console.log(localEvents);
+        setEvents(localEvents);
+    }, []);
 
     const styles = StyleSheet.create({
         container: {
@@ -25,12 +67,34 @@ export default function CalendarEvents() {
             borderWidth: 1,
             marginLeft: 10,
             marginRight: 10,
-            justifyContent: "center"
+        },
+        containerDiff:{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center"
         },
         textCurrentMonth: {
             fontSize: 24
         },
 
+        dropdown: {
+            width: 180,                     // фиксированная ширина поля :contentReference[oaicite:0]{index=0}  
+            backgroundColor: '#FFFFFF',
+            padding: 10,  
+            borderWidth: 1,                 // рамка  
+            borderColor: 'black',         // цвет рамки  
+            borderRadius: 6,                // скругления углов  
+            paddingVertical: 8,             // внутр. отступ по вертикали  
+            paddingHorizontal: 12,          // внутр. отступ по горизонтали  
+        },
+
+        dropdownPanel: {                 // ограничиваем высоту списка :contentReference[oaicite:1]{index=1}               // включаем скролл  
+            backgroundColor: '#FFFFFF',
+            padding: 10,
+            borderRadius: 6,
+            marginTop: 4,
+        },
 
         containerCalendar:{
             borderWidth: 1,
@@ -39,20 +103,35 @@ export default function CalendarEvents() {
         }
     })
 
-    type ItemProps = {title: string};
-
-    const Item = ({title}: ItemProps) => (
-        <View>
-            <Text>{title}</Text>
-        </View>
-    );
 
     return(
         <View style={styles.container}>
             <View style={styles.containerCurrentMonth}>
-                <View>
-                    <Text style={styles.textCurrentMonth}>{date}</Text>
-                    {/* <FlatList data={ivcArr} renderItem={(ItemIn) => <Item title={ItemIn.item}/>}/> */}
+                <View style={styles.containerDiff}>
+                    <Text style={styles.textCurrentMonth}>{selectedMonth}</Text>
+                    <Dropdown 
+                        style={styles.dropdown} panelStyle={styles.dropdownPanel} 
+                        value={selectedMonth} 
+                        onChange={(e) => setSelectedMonth(e.value)} 
+                        options={yearAndMonth} 
+                        placeholder="Месяц, год" />
+
+                    <Dropdown style={styles.dropdown} panelStyle={styles.dropdownPanel} 
+                        value={selectedIVC} 
+                        onChange={(e) => setSelectedIVC(e.value)} 
+                        options={ivcArr} placeholder="ИВЦ" />
+
+                    <MultiSelect 
+                        value={selectedEvent}
+                        options={events} 
+                        onChange={(e) => {
+                            setSelectedEvents(e.value);
+                        }} 
+                        optionLabel="label" 
+                        optionGroupLabel="label" 
+                        optionGroupChildren="items" 
+                        placeholder="Выбор мероприятия" 
+                        style={styles.dropdown} panelStyle={styles.dropdownPanel} />
                 </View>
             </View>
             <View style={styles.containerCalendar}>
--- packages/frontend/app/_layout.tsx
+++ packages/frontend/app/_layout.tsx
@@ -2,6 +2,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import authForm from './(auth)/auth-form';
 import CalendarEvents from './(calendar)/calendar';
+import { PrimeReactProvider } from "primereact/api"
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -13,10 +14,12 @@ export default function RootLayout() {
 
   return (
     <>
-      <Stack.Navigator initialRouteName='Calendar'>
-          <Stack.Screen name='Auth' component={authForm} options={{headerShown: false}}/>
-          <Stack.Screen name='Calendar' component={CalendarEvents} options={{headerShown: false}}/>
-        </Stack.Navigator>
+      <PrimeReactProvider>
+        <Stack.Navigator initialRouteName='Calendar'>
+            <Stack.Screen name='Auth' component={authForm} options={{headerShown: false}}/>
+            <Stack.Screen name='Calendar' component={CalendarEvents} options={{headerShown: false}}/>
+          </Stack.Navigator>
+      </PrimeReactProvider>
     </>
   );
 }
